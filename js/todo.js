baca726e5f719e519baaaec6d5655240594c80a3
Somsubhra <das.somsubhra1@gmail.com>
2018-07-21 16:36:38 +0530

    Initial commit

---
+++ js/todo.js
@@ -0,0 +1,90 @@
+// Create text node :
+
+// var h = document.createElement('h1')
+// var myValue = document.createTextNode('Hello World!')
+// h.appendChild(myValue)
+// document.querySelector('h1').appendChild(h)
+
+// While loop :
+
+// let val = 5
+
+// while (val > 0) {
+//     console.log(val);
+//     val--;
+    
+// }
+
+
+var ul = document.getElementById('list')
+var li
+
+
+var addButton = document.getElementById('add')
+addButton.addEventListener('click', addItem)
+
+var removeButton = document.getElementById('remove')
+removeButton.addEventListener('click', removeItem)
+
+var removeAllButton = document.getElementById('removeall')
+removeAllButton.addEventListener('click', removeAllItem)
+
+
+function addItem() {
+    var input = document.getElementById('input')
+    var item = input.value
+    ul = document.getElementById('list')
+    var textnode = document.createTextNode(item)
+
+    if (item === '') {
+        return false;
+        // const newPara = document.createElement('p')
+        // newPara.textContent = 'Enter your Todo'
+        // document.querySelector('h1').appendChild(newPara)
+        // console.log('test');
+        
+    } else {
+        //create li
+        li = document.createElement('li')
+        //create checkbox
+        var checkbox = document.createElement('input')
+        checkbox.type = 'checkbox'
+        checkbox.setAttribute('id', 'check')
+
+        // create label
+        var label = document.createElement('label')
+        label.setAttribute('for', 'item') //optional
+    }
+
+    //add to web page:
+
+    // ul.appendChild(label)
+    li.appendChild(checkbox)
+    label.appendChild(textnode)
+    li.appendChild(label)
+    ul.insertBefore(li, ul.childNodes[0])
+    
+
+    setTimeout(() => {
+        li.className = 'visual'
+    }, 2);
+
+    input.value = ''
+        
+}
+
+function removeItem() {
+    li = ul.children
+    for (let index = 0; index < li.length; index++) {
+        while (li[index] && li[index].children[0].checked) {
+            ul.removeChild(li[index])            
+        }
+    }
+}
+
+function removeAllItem() {
+    li = ul.children
+    for (let index = 0; index < li.length;) {
+        ul.removeChild(li[0])
+    }
+}
